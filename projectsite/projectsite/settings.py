8e79ab6162213cac0ac1f43b7e0e99fef6a20ae4
Norkizah <202080016@psu.palawan.edu.ph>
2024-04-09 16:47:09 +0800

    modified allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zaka3@mao%#z5=kxesex*ngw-k9ksrby!zk=ny3*6+(=nlu-^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
