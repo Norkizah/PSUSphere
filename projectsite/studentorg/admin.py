72bb238f244ba87059987c36c21ccf7df185e76f
Norkizah <202080016@psu.palawan.edu.ph>
2024-04-12 13:40:07 +0800

    refactor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here..
 from .models import College, Program, Organization, Student, OrgMember
 
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
+
 
 @admin.register(Student)
 class StudentAdmin (admin.ModelAdmin):
