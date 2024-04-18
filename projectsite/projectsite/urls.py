f9653c37cd19956865de86431580623532c82d2e
Norkizah <202080016@psu.palawan.edu.ph>
2024-04-19 07:31:58 +0800

    Fixed bugs

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
+from studentorg import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
+    path('',views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>' ,OrganizationUpdateView.as_view(), name='organization-update'),
